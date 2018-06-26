56b18729892e743979ec21dffde0ed45bc722ca4
DoctorFart1 <lukemueller@charter.net>
2018-06-26 00:43:52 -0500

    mew

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var port = process.env.PORT || 3000;
 
 
 var UserIdCounter = 0;
-//var lastUpdate = new Date().getTime();
+var players = [];
+var lastUpdate = new Date().getTime();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -16,22 +17,35 @@ io.on('connection', function(socket){
   socket.userID = UserIdCounter;
   io.emit('NewUserID', socket.userID);
 
-  socket.on('disconnect', function() {
 
+  socket.on('newUser', function(userID, gameID) {
+    var player = {};
+    player.userID = userID;
+    player.gameID = gameID;
+    players[players.length] = player;
+  });
+
+  socket.on('disconnect', function() {
+    for(i = 0; i < players.length; i++) {
+      if(players[i].userID === socket.userID) {
+        players.splice(i, 1);
+      }
+    }
   });
 
 });
 
 
-/*function Update() {
+function Update() {
   if(lastUpdate + 40 <= new Date().getTime()) {
+    io.emit('Update', players);
 
     lastUpdate = new Date().getTime();
   }
   setTimeout(function() {Update();}, 2);
 }
 
-Update();*/
+Update();
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
